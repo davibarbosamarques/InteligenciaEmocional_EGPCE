d0bb60aa12ca7c9852b3971d3512951496419b6a
Jayron <jayroncontax@gmail.com>
2023-11-10 16:22:31 -0300

    primeiro commit da branch

---
+++ Modulo01/index.php
@@ -0,0 +1,63 @@
+<!-- heade -->
+<?php
+    include( 'layout/head.php' );
+    ?>
+
+<body>
+    <!-- nav -->
+    <?php
+    include( 'layout/nav.php' );
+    ?>
+
+    <!-- menu -->
+    <?php
+    include( 'layout/menu.php');
+    ?>
+
+
+    <div class="all-conteudo">
+        <section>
+            <div class="main-content container-fluid">
+
+                <!-- imagem  -->
+                <div class="row">
+                    <!-- Begin row -->
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10 col-sm-8 container-fluid">
+
+                        <!-- Título -->
+                        <h2 class="Titulo fw-bolde scrool">Apresentação</h2>
+                        <!-- Título -->
+
+                        
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+        <!-- Botões de navegação -->
+        <div class="text-center">
+            <a href="topico01.php" class="btn btn-outline-success" role="button" aria-pressed="true">Próximo <i
+                    class="fa-solid fa-chevron-right"></i></a>
+        </div>
+
+        <!-- barra de progresso embaixo -->
+        <div id="progress-bar"></div>
+    </div>
+
+    <!-- BUTTON BACK TO TOP (JAMILLE) -->
+    <div id="back_to_top">
+        <div id="btn_back">
+            <i id="icon_up" class="fa-solid fa-arrow-up"></i>
+        </div>
+    </div>
+    <?php
+    include('layout/footer.php');
+    ?>
+</body>
+
+</html>
