19ac2a84eaae858e7dbfcffd76ffe6ba40677e78
davibarbosamarques <dbarbosamarques100@gmail.com>
2023-11-22 15:30:43 -0300

    volto já

--- Modulo02/index.php
+++ Modulo02/index.php
@@ -30,9 +30,9 @@
                         <!-- Título -->
 
                         <div class="tudo">
-                            <div class="row">
-                                <div class="col-sm-12 col-md-5 img-index d-flex align-items-center mb-5">
-                                    <img src="imgs/img-comeco.png" alt="Imagem que remete a organização" id="img-1" class="w-50 wow animate__zoomIn img-apresentacao">
+                            <div class="row d-flex align-items-center justify-content-between">
+                                <div class="col-sm-12 col-md-5 img-index">
+                                    <img src="imgs/img-comeco.png" alt="Imagem que remete a organização" id="img-1" class="w-75 wow animate__zoomIn img-apresentacao">
                                 </div>
 
                                 <div class="col-sm-12 col-md-7 mb-5">
