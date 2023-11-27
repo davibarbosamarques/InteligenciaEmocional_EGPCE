f272ca0f13399c08bc94448b4c56f8fd04a65a47
Jayron <jayroncontax@gmail.com>
2023-11-27 15:05:48 -0300

    topico02 finalizado!

--- Modulo02/layout/footer.php
+++ Modulo02/layout/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <hr>
-    <div class="text-center scrool  ">
+    <div class="text-center scrool wow fadeIn" data-wow-delay="0.3s">
         <p class="text">
             <?php echo date('Y'); ?> EGPCE | Escola de Gestão Pública do Estado do Ceará
         </p>
